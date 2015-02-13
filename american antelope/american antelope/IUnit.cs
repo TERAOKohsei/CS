aafa6733d4c06d46ba068485e11d44c00aaefa62
TERAO Kohsei <tera@chuo.co.jp>
2015-02-14 00:32:00 +0900

    CS.Common名前空間にIUnitインターフェイスを追加

---
+++ american antelope/american antelope/IUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Common {
+    /// <summary>
+    /// 装置としての機能を提供する。
+    /// </summary>
+    public interface IUnit : IDisposable {
+        /// <summary>
+        /// 装置との接続が確立しているかを取得する。
+        /// </summary>
+        bool IsConnected { get; }
+        /// <summary>
+        /// 装置と接続する。
+        /// </summary>
+        void Connect();
+        /// <summary>
+        /// 装置との接続を解除する。
+        /// </summary>
+        void Disconnect();
+    }
+}
