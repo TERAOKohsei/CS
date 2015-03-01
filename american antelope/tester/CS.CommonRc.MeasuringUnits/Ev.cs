8c1f9bf6a63d1c79747b856dec3e886a3279ffad
TERAO Kohsei <tera@chuo.co.jp>
2015-03-01 09:12:26 +0900

    IMeasureingUnitの見直し

--- american antelope/tester/CS.CommonRc.MeasuringUnits/Ev.cs
+++ american antelope/tester/CS.CommonRc.MeasuringUnits/Ev.cs
@@ -6,31 +6,76 @@ using System.Threading.Tasks;
 
 namespace CS.CommonRc.MeasuringUnits {
     class Ev : IMeasuringUnit {
+
+        #region コンストラクタ/デストラクタ
+        private Ev() : this(null) {
+        }
+
+        public Ev(params string[] sensorNames) {
+            sensorNames = new string[displacementCountValue];
+            displacementCountValue = sensorNames.Length;
+            foreach ( var name in sensorNames.Select((v, i) => new { Value = v, Index = i }) ) {
+                sensorNames[name.Index] = name.Value;
+            }
+        }
+        
+        ~Ev() {
+            Dispose(false);
+        }
+        #endregion // コンストラクタ/デストラクタ
+
         #region IMeasuringUnit メンバー
 
+        public int AngularCount { get { return 0; } }
+
+        int displacementCountValue;
+        public int DisplacementCount { get { return displacementCountValue; } }
+
+        public int HumidityCount { get { return 0; } }
+
+        public int TemperatureCount { get { return 0; } }
+
+        public string[] GetAngularNames() { return null; }
+
+        string[] sensorNames;
+        public string[] GetDisplacementNames() { return sensorNames; }
+
+        public string[] GetHumidityNames() { return null; }
+
+        public string[] GetTemperatureNames() { return null; }
+
         public void Measure() {
-            System.Diagnostics.Debug.WriteLine("Measure");
+            throw new NotImplementedException();
         }
 
         public double[] GetValues() {
-            var values = new double[6];
+            throw new NotImplementedException();
+        }
 
-            for ( int i = 0; i < 6; ++i ) {
-                values[i] = i;
-            }
+        public double[] GetAngulars() { return null; }
 
-            System.Diagnostics.Debug.WriteLine("GetValues");
-            return values;
+        public double[] GetDisplacements() {
+            throw new NotImplementedException();
+        }
+
+        public double[] GetHumidities() { return null; }
+
+        public double[] GetTemperatures() { return null; }
+
+        public void SetMeasurementItems(int displacementCount = 0, int angularCount = 0, int temeratureCount = 0, int humidityCount = 0) {
+            throw new NotImplementedException();
         }
 
         #endregion
 
         #region IUnit メンバー
 
-        public bool IsConnected { get; private set; }
+        public bool IsConnected {
+            get { throw new NotImplementedException(); }
+        }
 
         public void Connect() {
-            IsConnected = true;
+            throw new NotImplementedException();
         }
 
         #endregion
@@ -38,6 +83,11 @@ namespace CS.CommonRc.MeasuringUnits {
         #region IDisposable メンバー
 
         public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
         }
 
         #endregion
--- american antelope/tester/CS.CommonRc.MeasuringUnits/IMeasuringUnit.cs
+++ american antelope/tester/CS.CommonRc.MeasuringUnits/IMeasuringUnit.cs
@@ -8,7 +8,20 @@ using CS.Common;
 
 namespace CS.CommonRc.MeasuringUnits {
     interface IMeasuringUnit : IUnit {
+        int AngularCount { get; }
+        int DisplacementCount { get; }
+        int HumidityCount { get; }
+        int TemperatureCount { get; }
+        string[] GetAngularNames();
+        string[] GetDisplacementNames();
+        string[] GetHumidityNames();
+        string[] GetTemperatureNames();
         void Measure();
         double[] GetValues();
+        double[] GetAngulars();
+        double[] GetDisplacements();
+        double[] GetHumidities();
+        double[] GetTemperatures();
+        void SetMeasurementItems(int displacementCount = 0, int angularCount = 0, int temeratureCount = 0, int humidityCount = 0);
     }
 }
